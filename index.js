959099b901a81e97d1299cd848e8b77559a3bd5c
wolfix1234 <130220073+wolfix1234@users.noreply.github.com>
2025-09-13 11:44:44 +0330

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 
     process.env.NODE_ENV === 'production' 
-      ? 'https://oujamlak.com, https://oujamlak.ir' 
+      ? 'https://oujamlak.ir, https://oujamlak.com' 
       : 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
